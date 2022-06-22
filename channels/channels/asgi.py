2e5a2fa957e5d01952bd846b7b95e937d04b3a59
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-06-22 05:46:04 +0100

    django-channels

--- channels/channels/asgi.py
+++ channels/channels/asgi.py
@@ -1,28 +1,24 @@
-"""
-ASGI config for channels project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
-    # videocall/asgi.py
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channels.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-# application = get_asgi_application()
+import chat.routing
+
 application = ProtocolTypeRouter({
-"http": get_asgi_application(),
-"websocket": AuthMiddlewareStack(
-    URLRouter(
-        chat.routing.websocket_urlpatterns
-    )
-),
+  "http": get_asgi_application,
+  "websocket": 
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        
+    ),
 })
